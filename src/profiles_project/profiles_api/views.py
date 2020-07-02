580aba53fabc2c1d46b703854939b1bbef7a94d5
adityasuman2025 <aditya.me16@iitp.ac.in>
2020-07-02 14:07:04 +0530

    created and mapped a basic api view to browser url(get rqst)

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 # Create your views here.
+
+
+class HelloApiView( APIView ):
+	"""Test API View"""
+
+	def get( self, request, format = None ):
+		"""Returns a list of APIView features"""
+
+		an_apiview = [
+			'Uses HTTP methods as function i.e ( get, post, patch, put, delete )',
+			'It is similar to a traditional Django view',
+			'Gives me the most control of my logic',
+			'Is mapped manually to URLs'
+		]
+
+		return Response( { 'message': "Hello bro!", 'an_api_view': an_apiview } )
