7d168f0a2e5ab05ab6037f90655c113fb41a3e0c
adityasuman2025 <aditya.me16@iitp.ac.in>
2020-07-02 16:52:55 +0530

    crete, retrive, update, partial_update, destroy added in view set

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -59,13 +59,49 @@ class HelloApiView( APIView ):
 class HelloViewSet( viewsets.ViewSet ):
 	"""Testing View Sets"""
 
+	serializer_class = serializers.HelloSerializer # verify/test user's input
+
 	def list( self, request ):
 		"""Returns a hello message"""
 
 		a_viewset = [
-			"Uses actions (list, create, retrive, update, partial_update, destory)",
+			"Uses actions (list, create, retrive, update, partial_update, destroy)",
 			"Automatically maps to URLs using Routers",
 			"Provides more functionality with less code",
 		]
 
 		return Response( { 'message': "Hello!", 'a_viewset': a_viewset } )
+
+	def create( self, request ):
+		"""create a new hello messgae with our name"""
+
+		serializer = serializers.HelloSerializer( data = request.data )
+
+		if serializer.is_valid():
+			name = serializer.data.get( "name" )
+			message = "Hello " + name
+
+			return Response( { 'message': message } )
+		else:
+			return Response( serializer.errors, status = status.HTTP_400_BAD_REQUEST )
+
+	def retrieve( self, request, pk = None ):
+		"""handles getting an object by its ID"""
+
+		return Response( { 'http_method': 'GET' } )
+
+	def update( self, request, pk = None ):
+		"""handles updating an object"""
+
+		return Response( { 'http_method': 'put' } )
+
+	def partial_update( self, request, pk = None ):
+		"""handles updating only fields provided in the rqst"""
+
+		return Response( { 'http_method': 'patch' } )
+
+	def destroy( self, request, pk = None ):
+		"""handles deleting an object"""
+
+		return Response( { 'http_method': 'delete' } )
+
