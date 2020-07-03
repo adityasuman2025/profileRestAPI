5228b40c674077bbd9cdb2f7ee708fb0a56e5d0b
adityasuman2025 <aditya.me16@iitp.ac.in>
2020-07-03 13:16:31 +0530

    added permissions and authentication to our UserProfileViewSet to update profile of logged user only

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -4,7 +4,9 @@ from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
 
+from . import permissions
 from . import serializers
 from . import models
 
@@ -113,4 +115,6 @@ class UserProfileViewSet( viewsets.ModelViewSet ):
 	serializer_class = serializers.UserProfileSerializer
 	queryset = models.UserProfile.objects.all()
 
+	authentication_classes = ( TokenAuthentication, )
+	permission_classes = ( permissions.UpdateOwnProfile, )
 	
