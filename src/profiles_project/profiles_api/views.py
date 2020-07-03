4daa110e61c2892ad0af5f7fd3f933ff17f80a7c
adityasuman2025 <aditya.me16@iitp.ac.in>
2020-07-03 13:23:04 +0530

    added search feature in profile api

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -5,13 +5,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from . import permissions
 from . import serializers
 from . import models
 
 # Create your views here.
-
 
 class HelloApiView( APIView ):
 	"""Testing API View"""
@@ -117,4 +117,5 @@ class UserProfileViewSet( viewsets.ModelViewSet ):
 
 	authentication_classes = ( TokenAuthentication, )
 	permission_classes = ( permissions.UpdateOwnProfile, )
-	
+	filter_backends = ( filters.SearchFilter, )
+	search_fields = ( "name", "email", )
